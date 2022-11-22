2574d36969d1aecd88eb042c5172cf4f7bb13ae7
Manuka <manukajayarathne.coma@gmail.com>
2022-11-22 13:33:36 +0530

    created dbConnection

---
+++ src/db/dbConnection.java
@@ -0,0 +1,25 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+    private static dbConnection dbconnection = null;
+    private Connection connection;
+
+    private dbConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/d4", "root", "1234");
+    }
+
+    public static dbConnection getInstance() throws SQLException, ClassNotFoundException {
+        return (dbconnection==null)? dbconnection= new dbConnection(): dbconnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+
+}
